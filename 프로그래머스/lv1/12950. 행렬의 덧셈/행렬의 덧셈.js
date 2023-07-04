09a0ee99c6957f51873a07c93f49eafc1784f400
Hyungseob Kim <supremepilot@naver.com>
2023-07-04 15:55:23 +0900

    [level 1] Title: 행렬의 덧셈, Time: 11.00 ms, Memory: 67.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,10 +1,6 @@
-function solution(arr1, arr2) {
-    var answer = [[]];
-    for (var i=0; i<arr1.length; i++){
-        answer[i] =[];
-        for(var j=0; j<arr1[i].length; j++){
-            answer[i].push(arr1[i][j] + arr2[i][j]);
-        }
-    }
-    return answer;
+function solution(A, B) {
+    // console.log(arr1.map((arr1, idx1) => console.log(arr1); console.log(idx1)))
+    // console.log(A.map((arr1, idx1) => arr1.map((val, idx2) => val+B[idx1][idx2])));
+    return A.map((arr1, idx1) => arr1.map((val, idx2) => val+B[idx1][idx2]));
+    // return answer;
 }
