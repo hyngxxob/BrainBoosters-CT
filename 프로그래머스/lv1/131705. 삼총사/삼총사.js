b7dc43d9bb74b3c19c6c246f74cf2e0facc20257
Hyungseob Kim <supremepilot@naver.com>
2023-07-06 14:16:53 +0900

    [level 1] Title: 삼총사, Time: 0.24 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,13 +1,16 @@
 function solution(number) {
-    var answer = 0;
-    for(var i=0; i<number.length; i++){
-        for(var j=i+1; j<number.length; j++){
-            for(var k=j+1; k<number.length; k++){
-                if(number[i] + number[j] + number[k] === 0){
-                    answer += 1;
-                }
-            }
+    let result = 0;
+
+    const combination = (current, start) => {
+        if (current.length === 3) {
+            result += current.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+            return;
+        }
+
+        for (let i = start; i < number.length; i++) {
+            combination([...current, number[i]], i + 1);
         }
     }
-    return answer;
+    combination([], 0);
+    return result;
 }
